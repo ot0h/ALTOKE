0087c5ceaabcb99ab9c66705694cba4cb8969fe5
ot0h <rimv715@gmail.com>
2026-08-24 11:44:52 -0600

    probando el font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import { Montserrat_400Regular } from '@expo-google-fonts/montserrat'
+import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  useFonts({ Montserrat_400Regular })
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>El dizque font</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -16,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Montserrat_400Regular',
+    fontWeight: '700',
+    fontStyle: 'normal',
+    fontSize: 30,
   },
 })
