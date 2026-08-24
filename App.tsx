fb03a6d8d17f8d46005a75f21695157430e6c80c
ot0h <rimv715@gmail.com>
2026-08-24 15:35:37 -0600

    agregando el handler por si no llega a renderizar bein el font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { MontserratAlternates_400Regular } from '@expo-google-fonts/montserrat-alternates'
+import {
+  MontserratAlternates_400Regular,
+  MontserratAlternates_600SemiBold,
+} from '@expo-google-fonts/montserrat-alternates'
 import { StackNavigator } from '@navigation/StackNavigator'
 import { NavigationContainer } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import { JSX } from 'react'
+import { JSX, useEffect } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
 
-export default function App(): JSX.Element {
-  useFonts({ MontserratAlternates_400Regular })
+export default function App(): JSX.Element | null {
+  const [loaded, error] = useFonts({
+    MontserratAlternates_400Regular,
+    MontserratAlternates_600SemiBold,
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <NavigationContainer>
       <StackNavigator />
