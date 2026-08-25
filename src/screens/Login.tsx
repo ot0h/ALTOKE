ae86bf99086e31d3ebce32454400c15da31a289d
ot0h <rimv715@gmail.com>
2026-08-25 11:30:36 -0600

    feat: Agregando un padding left al placeholder el Custom Input, y metiendole un marginBottom a las card para que quedara dizque algo centrado

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 18,
     elevation: 8,
+    marginBottom: '25%',
   },
 
   containerInputs: {
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -94,6 +94,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 18,
     elevation: 8,
+    marginBottom: '25%',
   },
 
   containerInputs: {
