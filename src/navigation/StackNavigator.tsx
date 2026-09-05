1cd03e90acc317ae959bc7b6308edecd21d910ae
byrontorres26 <byrontorres@unitec.edu>
2026-09-05 15:03:49 -0600

    Agregando el Safe area y la pantalla Mis Comunidades

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Home, Login, Register } from '@screens'
 import { TabNavigator } from './TabsNavigator'
 import { DashboardStats } from '../components/dashboard/DashboardStats'
+import { MyCommunity } from '../screens/MyCommunity'
 
 export type RootStackParamList = {
   Home: undefined
@@ -21,7 +22,7 @@ export const StackNavigator = () => {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={MyCommunity} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="MainTabs" component={TabNavigator} />
